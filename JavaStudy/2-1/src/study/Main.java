0646fac4ffaa1bdf81de0ee6f44151b84cb93a64
hamada <hamashun18@yahoo.co.jp>
2020-06-26 14:23:43 +0900

    mainメソッド・インスタンス・メソッドの呼び出し
    課題提出

--- JavaStudy/2-1/src/study/Main.java
+++ JavaStudy/2-1/src/study/Main.java
@@ -1,10 +1,12 @@
 package study;
 
 /**
+ * Main.java
  *
- * 本課題では、フィールド変数、メソッドの基本的な使い方を学んでいきましょう。
+ * 本課題では、mainメソッド・インスタンス・メソッドの呼び出しの使い方を学んでいきましょう。
  * 課題範囲は以下のとおりです。
- *   Main.java: 問①から問③
+ *   Study.java: 問①
+ *   Main.java: 問①から問②
  * 指定された値と変数名を守って記述してください。
  *
  */
@@ -12,38 +14,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		//① firstNameとlastNameという名前の変数を定義し、
-		//   firstNameには自分の名前、lastNameには自分の名字で初期化しなさい
-		String firstName = "shun";
-		String lastName = "hamada";
+		// ① 以下のルールに従いAccountクラスのインスタンスを生成してください。
+		// 変数名:「account」
+		Account account = new Account("hoge", 123, "abc123");
 
-		//getName関数の呼び出しと出力
-		System.out.println(getName(firstName, lastName));
-		System.out.println();
+		// ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
+		account.printAccountInfo();
 
-		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-		//③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
-		//   要素が奇数の場合には「nは奇数です。」と出力されます。
-		for (int i = 0; i < arr.length; i++) {
-			isOdd(arr[i]);
-		}
-
-	}
-
-	//② 関数を定義しなさい。
-	//   関数名：getName
-	//  修飾子：public
-	//   引数には①で定義したfirstNameとlastNameを引数で受け取り、
-	//  last_name と first_name を連結した値を返り値とする変数を定義しなさい。
-	public static String getName(String first_name, String last_name) {
-		return last_name + first_name;
-	}
-
-	//奇数の判定を行う。
-	public static void isOdd(int num) {
-		if (num % 2 != 0) {
-			System.out.println(num + "は奇数です。");
-		}
 	}
 }
