ff2ffe7053689c8476d939fee486dbf6f52a47d0
hamada <hamashun18@yahoo.co.jp>
2020-06-26 22:18:09 +0900

    パッケージ
    課題提出

---
+++ JavaStudy/2-4/src/study/Study.java
@@ -0,0 +1,23 @@
+package study;
+
+/**
+ * 本課題では、基本的なJavaの構造と、importについて理解を深めていきましょう。
+ *
+ * 問①〜問③まであります。
+ * 全て回答し、課題にある画像と同じ表示になるようにしてください。
+ *
+ */
+public class Study {
+
+	public static void main(String[] args) {
+
+		/* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
+		 *     また、課題の画像と同じ表示になるように
+		 *     printFruitsの引数にはFruitsConstantsから呼び出した値を設定してください。
+		 */
+		fruits.Fruits.printFruits("レモン", 20);
+
+
+
+	}
+}
