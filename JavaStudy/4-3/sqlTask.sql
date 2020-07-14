da145009a7a513d0dc0043bd152554a99bd136ab
hamada <hamashun18@yahoo.co.jp>
2020-07-14 21:36:08 +0900

    SQL課題

---
+++ JavaStudy/4-3/sqlTask.sql
@@ -0,0 +1,53 @@
+/* ■ [回答]と記載のある箇所へ、1〜8の各課題内容に沿ったSQL文を記述しなさい。 */
+
+-- 1. 店舗テーブルから店舗名を抽出しなさい。また、列名の表示は別名で'店舗名'とすること。
+-- [回答]
+SELECT store_name AS 店舗名
+FROM store_table
+
+-- 2. 店舗情報を店舗名のABC順に抽出しなさい。
+-- [回答]
+SELECT *
+FROM store_table
+ORDER BY store_nameabc
+
+-- 3. 在庫テーブルに店舗テーブル、商品テーブルを「内部結合」し、店舗名・商品名・在庫数を全て取得しなさい。
+-- [回答]
+SELECT store_name 店舗名,goods_name 商品名,quantity 在庫数 FROM stock_table 在庫テーブル
+JOIN goods_table 商品テーブル
+ON 在庫テーブル.goods_code = 商品テーブル.goods_code
+JOIN store_table 店舗テーブル
+ON 在庫テーブル.store_code = 店舗テーブル.store_code
+
+-- 4. 商品テーブルから全商品の単価の平均値を抽出しなさい。
+-- [回答]
+SELECT AVG(price) FROM goods_table
+
+-- 5. 店舗コード='EA01'の在庫数の平均値より大きい在庫数を持つ店舗コードを抽出しなさい。
+-- [回答]
+SELECT store_code 店舗コード FROM stock_table
+GROUP BY store_code
+HAVING 
+AVG(quantity) > (SELECT AVG(quantity) 
+				 FROM stock_table 
+				 WHERE store_code='EA01')
+
+-- 6. 商品テーブルに「商品コード='M001'、商品名='マフラー'、単価=4500円、更新日付=本日日付」のデータを追加しなさい。※実行後のSELECT結果も貼付すること。
+-- [回答]
+INSERT INTO goods_table(
+goods_code, goods_name, price, update_day) 
+VALUES('M001','マフラー',4500,'2020-07-14')
+
+-- 7. 在庫テーブルの商品コード='S987'、かつ、店舗コード='EA01'に対して、「在庫数=10、更新日付=本日日付」で更新しなさい。※実行後のSELECT結果も貼付すること。
+-- [回答]
+UPDATE stock_table
+SET quantity = 10,update_day = '2020-07-14'
+WHERE goods_code='S987' AND store_code = 'EA01'
+
+-- 8. 7で更新した商品を削除しなさい。※実行後のSELECT結果も貼付すること。
+-- [回答]
+DELETE FROM stock_table
+WHERE goods_code='S987' 
+	  AND store_code='EA01'
+	  AND quantity=10
+	  AND update_day='2020/07/14'
